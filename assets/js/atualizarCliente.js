70a370b93ed94ae0213481d8f5719978f585a43a
Xiaoying He <jessicahe333@hotmail.com>
2022-09-30 18:21:40 -0300

    Alerecao Geral

--- assets/js/atualizarCliente.js
+++ assets/js/atualizarCliente.js
@@ -18,11 +18,13 @@ function consultarClientePeloID(){
     liEmail = document.getElementById("li2");
     liCpf = document.getElementById("li3");
     liTelefone1 = document.getElementById("li4");
+    liSenha = document.getElementById("li5");
 
     liNome.innerHTML = "Nome: " + cliente.nome;
     liEmail.innerHTML = "E-mail: " + cliente.email;
     liCpf.innerHTML = "CPF: " + cliente.cpf;
     liTelefone1.innerHTML = "Telefone: " + cliente.telefone1;
+    liSenha.innerHTML = "Senha: " + cliente.senha;
 
 }
 
@@ -45,24 +47,22 @@ function fazerPut(url, body) {
 function atualizarCliente() {
     let id = document.getElementById('altera').value;
     let url = "http://127.0.0.1:5000/clientes";
-    let nome = document.getElementById('li1').value;
+    let nome = document.getElementById('nome').value;
     let email = document.getElementById('email').value;
     let cpf = document.getElementById('cpf').value;
     let tel = document.getElementById('tel').value;
     let senha = document.getElementById('senha').value;
     let mensagem = document.querySelector('p');
     
-    if(nome === '' || email === '' || cpf === ''|| tel === '' || senha === ''){
+    if(nome === '' || senha === ''/*|| tel === '' || senha === ''*/){
         event.preventDefault();
-        mensagem.textContent = "Preencher todos os campos por favor!";
+        mensagem.textContent = "Campos Nome e Senha são obrigatórios!";
         
     }
     else{
         event.preventDefault();
         mensagem.textContent = "Os dados do cliente atulizados com sucesso!!!";
-        /*setTimeout(function(){
-            window.location.href = 'dadosCliente.html';
-       }, 2000);*/
+       window.location.reload();
         
     }
   
@@ -76,5 +76,5 @@ function atualizarCliente() {
     }
 
     fazerPut (url, body);
-    window.location.reload();
+    
 }
